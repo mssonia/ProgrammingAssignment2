026713720b6d48c8e070f5340f00b9902b18e765
Sonia V. Smith <sonia_smith@verizon.net>
2014-08-23 18:31:53 -0400

    Initial submission of code for makeCacheMatrix() and cacheSolve() functions comprising cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Sonia Smith - RPROG6 - Programming Assignment 2
+## These functions cache a matrix to the global environment and calculates it inverse which is also cached globally.
 
-## Write a short comment describing this function
-
+## The makeCacheMatrix function create a matrix object, setter method, getter method, and method 
+## get and set inverse of matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverseMatrix <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        inverseMatrix <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    
+    setSolve <- function(solve) {
+        inverseMatrix <<- solve
+    }
+    
+    getSolve <- function() {
+        inverseMatrix
+    }
+    
+    list(set = set, 
+    get = get,
+    setSolve = setSolve,
+    getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
-
+## The cacheSolve function determines if mat
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    s <- x$getSolve()
+    
+    if(is.null(s)) {
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setSolve(s)    
+    }        
+    
+    s
 }
